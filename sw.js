2f98d4f85b440778b1365967c344f98ce5d4b4ef
trelewapps <bahiarapiman@gmail.com>
2026-04-06 19:31:06 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // === REGLA DE ORO: Cambia este número de versión cada vez que modifiques tu index.html ===
-const CACHE_NAME = 'predicacion-v11'; 
+const CACHE_NAME = 'predicacion-v12'; 
 
 const ASSETS_TO_CACHE = [
   './',
